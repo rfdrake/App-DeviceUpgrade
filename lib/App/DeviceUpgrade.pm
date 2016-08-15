3779f23bc3bfd0de0972a83f34f1c6eade42d654
Robert Drake <rdrake@direcpath.com>
2016-08-15 14:36:13 -0400

    release testing handled by TestingMania

--- lib/App/DeviceUpgrade.pm
+++ lib/App/DeviceUpgrade.pm
@@ -1,6 +1,26 @@
+use strict;
+use warnings;
+
 package App::DeviceUpgrade;
 
 
+=head1 NAME
 
-# this is just for versioning right now.  I might put common subs that are
-# shared between firmware scripts here.
+App::DeviceUpgrade - Scripts for upgrading firmware
+
+=head1 SYNOPSIS
+
+See the README file for instructions for the individual scripts
+
+=head1 AUTHOR
+
+Robert Drake, C<< <rdrake at cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2016 Robert Drake, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
